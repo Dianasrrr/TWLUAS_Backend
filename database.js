d10dac05712d612850b498af64bd0a012e25b1a7
Dianasrrr <dian2100016007@webmail.uad.ac.id>
2023-07-24 11:25:24 +0700

    update  backend

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-// Mengganti 'nama-database' dengan nama yang sesuai
-const databaseName = 'nama-database';
 
 // Mengganti 'mongodb://localhost:27017/' dengan URL MongoDB yang sesuai
-const mongoURL = `mongodb+srv://dian2100016007:dianpk@diantoro.dnrklpa.mongodb.com/responsi?retryWrites=true&w=majority`;
+const mongoURL = `mongodb+srv://dian2100016007:dianpk@diantoro.dnrklpa.mongodb.net/uas?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => {
         console.log('Connected to MongoDB');
     })
